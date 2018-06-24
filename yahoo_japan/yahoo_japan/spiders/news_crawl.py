f3a4ad584b92b35dc9bac09160522face46359e4
nkmk <nkmk.on@gmail.com>
2018-06-24 14:41:14 +0900

    generate spider for news_crawl

---
+++ yahoo_japan/yahoo_japan/spiders/news_crawl.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+
+class NewsCrawlSpider(CrawlSpider):
+    name = 'news_crawl'
+    allowed_domains = ['yahoo.co.jp']
+    start_urls = ['http://yahoo.co.jp/']
+
+    rules = (
+        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+    )
+
+    def parse_item(self, response):
+        i = {}
+        #i['domain_id'] = response.xpath('//input[@id="sid"]/@value').extract()
+        #i['name'] = response.xpath('//div[@id="name"]').extract()
+        #i['description'] = response.xpath('//div[@id="description"]').extract()
+        return i
